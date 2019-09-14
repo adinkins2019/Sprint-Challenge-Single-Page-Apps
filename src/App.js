67c7e86b5bf69103157ec28fc3be639d40727cf2
adinkins2019 <afdinki@gmail.com>
2019-09-14 13:30:59 -0400

    Added url for api and sent it as props to components to check data utilization

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-
+import LocationsList from './components/LocationsList'
+import EpisodeList from './components/EpisodeList'
+import CharacterList from './components/CharacterList'
 
 export default function App() {
+  const api = "https://rickandmortyapi.com/api/"
   return (
     <main>
       <Header />
       {/* <TabNav /> */}
+      <CharacterList api={api} />
+      <EpisodeList api={api} />
+      <LocationList api={api} />
     </main>
   );
 }
