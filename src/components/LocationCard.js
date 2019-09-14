ad2fec7e2e801ecf9c6e7237bccec52494b4ddd8
adinkins2019 <afdinki@gmail.com>
2019-09-14 14:17:39 -0400

    Modified LocationCard.js to render props inherited from LocationList

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+  console.log(residents)
+  return (
+    <div>
+      <h2>Location Name: {name}</h2>
+      <h3>Location Type: {type}</h3>
+      <h3>Dimension: {dimension}</h3>
+      <p>{residents.map(resident => <div>{resident}</div>)}</p>
+
+    </div>
+  )
 }
