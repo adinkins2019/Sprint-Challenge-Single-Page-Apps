7ed4ef1ede94f35672724594499d5954bf2631d6
adinkins2019 <afdinki@gmail.com>
2019-09-14 13:29:55 -0400

    Modified LocationsList to add call to api via axios GET request

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList({api}) {
   // TODO: Add useState to track data from useEffect
@@ -6,12 +7,16 @@ const [characters, setCharacters] = useState([])
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    
+      axios
+      .get(`${api}/characters`)
+      .then(response => console.log(response.data))
+      .catch(err => console.log('Error: ', err))
   }, [characters]);
 
   return (
     <section className="character-list grid-view">
       <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => console.log(character))}
     </section>
   );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,22 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
-export default function LocationsList() {}
+export default function LocationsList() {
+     // TODO: Add useState to track data from useEffect
+     const [locations, setLocations] = useState([])
+     useEffect(() => {
+       // TODO: Add API Request here - must run in `useEffect`
+       axios
+       .get(`${api}/location`)
+       .then(response => console.log(response.data))
+       .catch(err => console.log("Error: ", err))
+       //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+     }, [locations]);
+   
+     return (
+       <section className="location-list grid-view">
+         <h2>TODO: `array.map()` over your state here!</h2>
+         {locations.map(location => console.log(location)) }
+       </section>
+     );
+}
