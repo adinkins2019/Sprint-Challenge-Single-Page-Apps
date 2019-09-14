8268de03105246163cb2a01f7704b4badc70bc08
adinkins2019 <afdinki@gmail.com>
2019-09-14 14:15:29 -0400

    Modified LocationsList.js to display LocationCard info

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
+import LocationCard from './LocationCard'
 
 export default function LocationsList({api}) {
      // TODO: Add useState to track data from useEffect
@@ -8,15 +9,20 @@ export default function LocationsList({api}) {
        // TODO: Add API Request here - must run in `useEffect`
        axios
        .get(`${api}/location`)
-       .then(response => console.log(response.data.results))
+       .then(response => {
+           console.log(response.data.results)
+           setLocations(response.data.results)
+       })
        .catch(err => console.log("Error: ", err))
        //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
      }, [locations]);
    
      return (
        <section className="location-list grid-view">
-         <h2>TODO: `array.map()` over your state here!</h2>
-         {locations.map(location => console.log(location)) }
+         {/*<h2>TODO: `array.map()` over your state here!</h2>*/}gi
+         {locations.map(location => 
+            <LocationCard name={location.name} type={location.type} dimension={location.dimension} residents={location.residents}/>
+         )}
        </section>
      );
 }
