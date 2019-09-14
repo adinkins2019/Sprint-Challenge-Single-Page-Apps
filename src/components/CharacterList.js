6963ce40bf8fd14e71ecb3a4e5da86826dc1453e
adinkins2019 <afdinki@gmail.com>
2019-09-14 13:11:56 -0400

    Modified CharacterList.js to add state information and get data from api

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 
-export default function CharacterList() {
+export default function CharacterList({api}) {
   // TODO: Add useState to track data from useEffect
-
+const [characters, setCharacters] = useState([])
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    
+  }, [characters]);
 
   return (
     <section className="character-list grid-view">
