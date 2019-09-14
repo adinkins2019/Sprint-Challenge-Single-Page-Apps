1a16acec639230a37b96bb84b343415780bb9fd2
adinkins2019 <afdinki@gmail.com>
2019-09-14 14:44:07 -0400

    Modified CharacterCard.js to add characteristics of the character to the components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({created, episode, gender, image, location, name, origin, species, status, type, url}) {
+  return (
+    <div>
+      <img src={image} alt={`Picture of ${name}`}/>
+      <h2>{name}</h2>
+      <p>Created:{created}<br/>
+         Gender: {gender}<br/>
+         Species: {species}<br/>
+         Status: {status}<br/>
+         Type: {type}<br/>
+         Origin: {origin.name} <br/>
+         Location: {location.name}<br />
+         Url: {url}
+
+      </p>
+    </div>
+  );
 }
