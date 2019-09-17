ec27daaa60b48d6e369431c6b6fc29066a0f670b
adinkins2019 <afdinki@gmail.com>
2019-09-16 20:13:19 -0400

    Updated App.js to include NavLinks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import {Route } from 'react-router-dom'
+import {Route, NavLink } from 'react-router-dom'
 import LocationsList from './components/LocationsList'
 import EpisodeList from './components/EpisodeList'
 import CharacterList from './components/CharacterList'
 import WelcomePage from './components/WelcomePage'
 
 export default function App() {
-  
+
   return (
     <main>
       <Header />
       {/* <TabNav /> */}
       <CharacterList />
       <EpisodeList />
-     
+     <NavLink to="/">Home</NavLink>
+     <NavLink to="/character">Characters</NavLink>
+     <NavLink to="/episode">Episodes</NavLink>
+     <NavLink to="/location">Locations</NavLink>
+
       <Route exact path="/" component={WelcomePage} />
       <Route path="/character" component={CharacterList} />
       <Route path="/location" component={LocationsList} />
